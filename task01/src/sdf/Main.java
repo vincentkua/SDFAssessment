93287b732ed0c79d7c9630edbc3b0c4a8bde6d6d
atlaskua <zylyn89@gmail.com>
2023-01-27 09:44:27 +0800

    readfilecomplete

--- task01/src/sdf/Main.java
+++ task01/src/sdf/Main.java
@@ -1,10 +1,51 @@
 package sdf;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
 
-        System.out.println("OK this works");
+        // System.out.println("OK this works");
+        if (args.length != 1) {
+            System.out.println("Please indicate the File Name");
+            System.exit(1);
+        }
+
+        String filename = args[0];
+        Path cithpath = Paths.get(filename);
+        File cith = cithpath.toFile();
+
+        FileReader fr = new FileReader(cith);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            // remove punctuations
+
+            line = line.replaceAll("\\.|\\,|\\:|\\!|\\?|\\-|\\(|\\)|\\{|\\}|\\'|\"", " ");
+            line = line.toUpperCase();
+            String[] wordsarray = line.trim().split(" ");
+            for(int i = 0 ; i < wordsarray.length ; i++){
+                String word = wordsarray[i];
+                if(!word.equals("")){
+                    System.out.println(word);
+                }
+
+
+
+            }
+
+
+
+        }
+
     }
 
 }
